2212a66f1bf8a39eafd14dd30a909deff647d6e1
James Miller <cask.zingy0o@icloud.com>
2025-11-24 19:30:56 -0800

    added 'LICENSE'

--- mat.h
+++ mat.h
@@ -1,5 +1,27 @@
 // Mat (MAth Tokenizer)
 
+/*
+  MIT License
+  Copyright (c) 2025 James Miller
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+  of the Software, and to permit persons to whom the Software is furnished to do
+  so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,8 +31,6 @@
 #define MAT_WARN   "\e[33mWarning:\e[0m "
 #define MAT_ERROR  "\e[1;31mError:\e[0m "
 #define mat_uint   unsigned int
-
-
 
 // From nob.h: https://github.com/tsoding/nob.h
 #ifdef __cplusplus
